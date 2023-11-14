670bd12b896cb345d3693dbae966fc17743036ef
simon <simoncherrygz@gmail.com>
2023-11-14 18:04:59 +0800

    fix(album): 解决相册页转场时目录列表 UI 出界问题

--- lib/presentation/album/widget/album_list_view.dart
+++ lib/presentation/album/widget/album_list_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_resume/presentation/album/album.dart';
 import 'package:photo_manager/photo_manager.dart';
 
-class AlbumListView extends StatelessWidget {
+class AlbumListView extends StatefulWidget {
   const AlbumListView({
     super.key,
     required this.paths,
@@ -14,31 +14,66 @@ class AlbumListView extends StatelessWidget {
   final bool showAlbums;
 
   @override
+  State<AlbumListView> createState() => _AlbumListViewState();
+}
+
+class _AlbumListViewState extends State<AlbumListView> {
+  bool _transitionCompleted = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final route = ModalRoute.of(context);
+    void handler(status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _transitionCompleted = true;
+        });
+      } else if (status == AnimationStatus.reverse) {
+        setState(() {
+          _transitionCompleted = false;
+        });
+      } else if (status == AnimationStatus.dismissed) {
+        route?.animation?.removeStatusListener(handler);
+        setState(() {
+          _transitionCompleted = true;
+        });
+      }
+    }
+
+    route?.animation?.addStatusListener(handler);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final bloc = context.read<AlbumBloc>();
     final screenHeight = MediaQuery.sizeOf(context).height;
-    return AnimatedSlide(
-      duration: const Duration(milliseconds: 300),
-      offset: Offset(0, showAlbums ? 0 : -1),
-      child: Container(
-        width: double.infinity,
-        height: screenHeight * 0.6,
-        color: Colors.white,
-        child: ListView.separated(
-          itemCount: paths.length,
-          itemBuilder: (context, index) {
-            final path = paths[index];
-            return ListTile(
-              title: Text(path.name),
-              onTap: () {
-                bloc.add(ToggleAlbums());
-                bloc.add(SelectAlbum(index));
-              },
-            );
-          },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
+    final offstage = !_transitionCompleted;
+    return Offstage(
+      offstage: offstage,
+      child: AnimatedSlide(
+        duration: const Duration(milliseconds: 300),
+        offset: Offset(0, widget.showAlbums ? 0 : -1),
+        child: Container(
+          width: double.infinity,
+          height: screenHeight * 0.6,
+          color: Colors.white,
+          child: ListView.separated(
+            itemCount: widget.paths.length,
+            itemBuilder: (context, index) {
+              final path = widget.paths[index];
+              return ListTile(
+                title: Text(path.name),
+                onTap: () {
+                  bloc.add(ToggleAlbums());
+                  bloc.add(SelectAlbum(index));
+                },
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const Divider();
+            },
+          ),
         ),
       ),
     );
